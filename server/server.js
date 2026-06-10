e108c29bf315382e9a3cdad84e971dcd44d75a19
Shifa04 <shifarabiya4216@gmail.com>
2026-06-10 16:04:29 +0530

    update transporter code

--- server/server.js
+++ server/server.js
@@ -43,11 +43,14 @@ mongoose.connect(process.env.DB)
 
 // Create Nodemailer transporter once here
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL,       
+    user: process.env.EMAIL,
     pass: process.env.PASS
-  }
+  },
+  connectionTimeout: 30000
 });
 
 // SIGNUP route
